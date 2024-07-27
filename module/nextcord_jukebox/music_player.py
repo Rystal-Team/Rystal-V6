94bf48bf74ba829de43b894ba6bdbd480d874625
Loco <61045140+Loco-CTO@users.noreply.github.com>
2024-07-27 02:58:12 +0100

    Update music_player.py

--- module/nextcord_jukebox/music_player.py
+++ module/nextcord_jukebox/music_player.py
@@ -377,7 +377,7 @@ class MusicPlayer:
 
         self.music_queue.append(song)
         if not self.paused and self.music_queue and not self._now_playing:
-            await asyncio.to_thread(self._play_func, None, self.music_queue[0])
+            await self._play_func(None, self.music_queue[0])
 
         print(colored(text=f"Time taken: {time.time() - timer}", color="dark_grey"))
         return song
