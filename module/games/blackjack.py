5365f21189aa985d44c0b95ef0a0fd0fa41087f5
Loco <61045140+Loco-CTO@users.noreply.github.com>
2024-08-16 21:41:10 +0100

    two decks

--- module/games/blackjack.py
+++ module/games/blackjack.py
@@ -43,7 +43,7 @@ class Blackjack:
         """
         Initialize the Blackjack game with a shuffled deck and empty hands for player and dealer.
         """
-        self.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
+        self.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 8
         random.shuffle(self.deck)
         self.player_hand = []
         self.dealer_hand = []
