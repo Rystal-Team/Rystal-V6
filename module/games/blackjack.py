7b44266b90b29cf434ae7cd84d8da2426a0b42e8
Loco <61045140+Loco-CTO@users.noreply.github.com>
2024-08-19 15:12:20 +0100

    Fixed: Dealer not hitting at all

--- module/games/blackjack.py
+++ module/games/blackjack.py
@@ -139,7 +139,11 @@ class Blackjack:
         self.dealer_hand[0] = self.dealer_first_card
         player_total = self.calculate_hand(self.player_hand)
         dealer_total = self.calculate_hand(self.dealer_hand)
-        if not self.player_black_jack and not player_total > 21 and dealer_total == 21:
+        if (
+            not self.player_black_jack
+            and not player_total > 21
+            and not dealer_total == 21
+        ):
             while self.calculate_hand(self.dealer_hand) < self.calculate_hand(
                 self.player_hand
             ):
